cd797913ff2fe0216b3178b53bc7e6c594af1c8d
Tomás Brito <tomasbrito7@gmail.com>
2025-12-11 19:54:55 +0000

    Criação de um gestor de aplicações

---
+++ App_manager/Request_App.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_manager
+{
+    internal class Request_App
+    {
+        [JsonProperty("res-type")]
+        public string ResType { get; set; }
+
+        [JsonProperty("resource-name")]
+        public string ResourceName { get; set; }
+    }
+}
